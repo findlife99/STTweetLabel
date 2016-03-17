2e0fc9fb76375f64d34e92d02bc440ed90172901
楊育宗 <b96902108@gmail.com>
2016-03-17 11:01:15 +0800

    detect two hashtags in a row

--- STTweetLabel/STTweetLabel.m
+++ STTweetLabel/STTweetLabel.m
@@ -193,9 +193,10 @@
 				break;
 		}
 
+        unichar tmp = [tmpText characterAtIndex:range.location];
 		[tmpText replaceCharactersInRange:range withString:@"%"];
 		// If the hot character is not preceded by a alphanumeric characater, ie email (sebastien@world.com)
-		if (range.location > 0 && [validCharactersSet characterIsMember:[tmpText characterAtIndex:range.location - 1]] && [tmpText characterAtIndex:range.location] != '#')
+		if (range.location > 0 && [validCharactersSet characterIsMember:[tmpText characterAtIndex:range.location - 1]] && tmp != '#')
 			continue;
 
 		// Determine the length of the hot word
